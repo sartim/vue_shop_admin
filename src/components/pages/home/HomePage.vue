64c085dbc8de7c80d7b2116832152c8b6af77e5a
Tim <sarrtim@gmail.com>
2019-05-03 17:11:49 +0300

    Add dashboard view design

--- src/components/pages/home/HomePage.vue
+++ src/components/pages/home/HomePage.vue
@@ -6,7 +6,61 @@
                 <left-side-nav></left-side-nav>
                 <section id="content">
                     <div class="container">
-                        <h1>Dashboard</h1>
+                        <div id="card-stats">
+                            <div class="row">
+                                <div class="col s12 m6 l2">
+                                    <div class="card">
+                                        <div class="card-content light-blue white-text">
+                                            <p class="card-stats-title" style="font-size: 12px">Orders Today</p>
+                                            <h4 class="card-stats-number">0</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l2">
+                                    <div class="card">
+                                        <div class="card-content light-blue lighten-1 white-text">
+                                            <p class="card-stats-title" style="font-size: 12px">Orders Yesterday</p>
+                                            <h4 class="card-stats-number">0</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l2">
+                                    <div class="card">
+                                        <div class="card-content light-blue white-text">
+                                            <p class="card-stats-title" style="font-size: 12px">Orders This Month</p>
+                                            <h4 class="card-stats-number">0</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l2">
+                                    <div class="card">
+                                        <div class="card-content light-blue white-text">
+                                            <p class="card-stats-title" style="font-size: 12px">Orders Last Month</p>
+                                            <h4 class="card-stats-number">0</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l2">
+                                    <div class="card">
+                                        <div class="card-content light-blue white-text">
+                                            <p class="card-stats-title" style="fontSize: 12px">Orders This Year</p>
+                                            <h4 class="card-stats-number">0</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col s12 m6 l2">
+                                    <div class="card">
+                                        <div class="card-content light-blue white-text">
+                                            <p class="card-stats-title" style="font-size: 12px">Orders Last Year</p>
+                                            <h4 class="card-stats-number">0</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row" style="background: #FFFFFF; padding: 0px">
+                        <div id="chartdiv" style="width: 100%; height: 500px"></div>
+                        </div>
                     </div>
                 </section>
             </div>
