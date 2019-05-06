731a3153343aa2752906f00ec66492f56c02cec8
tim <sarrtim@gmail.com>
2019-05-06 17:51:11 +0300

    Add socketio client config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import Vue from 'vue';
 
+import config from 'config';
 import { store } from './_store';
 import { router } from './_helpers';
 import App from './app/App';
+import VueSocketIO from 'vue-socket.io';
 
 // setup fake backend
 import { configureFakeBackend } from './_helpers';
 configureFakeBackend();
+
+Vue.use(new VueSocketIO({
+    debug: true,
+    connection: `${config.apiUrl}`,
+    vuex: {
+        store,
+        actionPrefix: 'SOCKET_',
+        mutationPrefix: 'SOCKET_'
+    },
+    // options: { path: "/my-app/" } //Optional options
+}));
 
 new Vue({
     el: '#app',
