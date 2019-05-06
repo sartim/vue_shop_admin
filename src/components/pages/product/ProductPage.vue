071916567f9237209a68d92abe01544e121877e9
tim <sarrtim@gmail.com>
2019-05-06 16:40:07 +0300

    Add table html

--- src/components/pages/product/ProductPage.vue
+++ src/components/pages/product/ProductPage.vue
@@ -5,7 +5,36 @@
             <div class="wrapper">
                 <left-side-nav></left-side-nav>
                 <section id="content">
-
+                    <div class="row">
+                        <div class="col s12">
+                            <table>
+                                <table class="responsive-table striped highlight">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>NAME</th>
+                                        <th>CATEGORY</th>
+                                        <th>QUANTITY</th>
+                                        <th>CREATED DATE</th>
+                                        <th>ACTION</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td>
+                                            <button class="btn">Manage</button>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </table>
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
