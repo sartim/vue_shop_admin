446dfdc2d84289ff5e892f1d52fa4267a8e6c718
tim <sarrtim@gmail.com>
2019-05-07 15:26:09 +0300

    Decrypt user data got from local storage to send as auth header

--- src/_helpers/auth-header.js
+++ src/_helpers/auth-header.js
@@ -1,7 +1,8 @@
+import {Script} from "./script";
+
 export function authHeader() {
     // return authorization header with jwt token
-    let user = JSON.parse(localStorage.getItem('user'));
-
+    let user = JSON.parse(Script.decrypt(localStorage.getItem('user')));
     if (user && user.access_token) {
         return { 'Authorization': 'Bearer ' + user.access_token };
     } else {
